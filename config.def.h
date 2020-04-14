bef2c6d99aee75f952ca32fb50b424043dbb3e1c
Christian Tenllado <ctenllado@gmail.com>
2020-04-14 23:31:15 +0200

    Multiple scratchpads
    This patch enables multiple scratchpads, each with one asigned window.
    This enables the same scratchpad workflow that you have in i3.
    
    Scratchpads are implemented as special tags, whose mask does not
    apply to new spawned windows. To assign a window to a scratchpad you
    have to set up a rule, as you do with regular tags.
    
    Windows tagged with scratchpad tags can be set floating or not in the
    rules array. Most users would probably want them floating (i3 style),
    but having them tiled does also perfectly work and might fit better the
    DWM approach. In case they are set floating, the patch moves them to the
    center of the screen whenever they are shown. The patch can easily be
    modified to make this last feature configurable in the rules array (see
    the center patch).
    
    The togglescratch function, borrowed from the previous scratchpad patch
    and slightly modified, can be used to spawn a registered scratchpad
    process or toggle its view. This function looks for a window tagged with
    the selected scratchpad tag. If it is found its view is toggled. If it is
    not found the corresponding registered command is spawned. The
    config.def.h shows three examples of its use to spawn a terminal in the
    first scratchpad tag, a second terminal running ranger on the second
    scratchpad tag and the keepassxc application to manage passwords on a
    third scratchpad tag.
    
    If you prefer to spawn your scratchpad applications from the startup
    script, you might opt for binding keys to toggleview instead, as
    scratchpads are just special tags (you may even extend the TAGKEYS macro
    to generalize the key bindings).

--- config.def.h
+++ config.def.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       "spterm",   NULL,	      SPTAG(0),	    1,           -1 },
 };
 
 /* layout(s) */
@@ -62,11 +63,24 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = {"st", "-n", "spcalc", "-g", "50x20", "-z", "16", "-e", "bc", "-lq", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spcalc",      spcmd2},
+};
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.ui = 0 } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
